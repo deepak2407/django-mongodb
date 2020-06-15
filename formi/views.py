92e2b93bd5e91d653a54d3bd899f91d297aa0b09
deepak <nvenkatadeepak@gmail.com>
2020-06-14 20:58:47 -0400

    connection established and data inserting with mongodb

--- formi/views.py
+++ formi/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from .models import sample_collection
 # Create your views here.
 from django.http import HttpResponse
-
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client['firstdb']
+collection = db.formi_sample_collection
 
 def index(request):
     return render(request, 'index.html')
@@ -18,5 +21,5 @@ def submit(request):
         return render(request,'index.html')
 
 def showData(request):
-    x=sample_collection.objects.filter(data='abc')
+    x=collection.count_documents({})
     return render(request, 'sucess.html', {'val':x})
