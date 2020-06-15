9b6a09a9e0b352c3b9e58d7e74ad02c3664c94c2
deepak <nvenkatadeepak@gmail.com>
2020-06-14 20:11:32 -0400

    connection established and data inserting with mongodb

---
+++ formi/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+# Create your models here.
+class sample_collection(models.Model):
+    data=models.TextField()
---
+++ formi/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .models import sample_collection
+# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def submit(request):
+    if request.method == 'GET':
+        post=sample_collection()
+        post.data= request.GET['name1']
+        post.save()
+        return render(request, 'sucess.html', {'val': post.data})  
+
+    else:
+        return render(request,'index.html')
+
+def showData(request):
+    return render(request, 'sucess.html')
